2c2dab9e27d97eb5e65d3b81e287bfdf54ce2983
EricPapell <ericpapell123@gmail.com>
2023-06-06 17:10:55 -0500

    added getRandomNumber function

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+const getRandomNumber = function(max,min){
+    return Math.trunc(Math.random(max-min)*max+1)
+}
+
+
 const getQuote = async function(){
     try{
         const response = await fetch('https://jacintodesign.github.io/quotes-api/data/quotes.json')
+        if(!response.ok) throw new Error('There is no such a link')
         const data = await response.json()
+        
         console.log(response)
         console.log(data)
+        const numR = getRandomNumber(data.length,1)
+        console.log(numR)
     }
     catch(err){
         console.error(err)
