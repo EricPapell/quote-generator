1005f6adc4b797c55edda0b4b16f17e579c4d28e
EricPapell <ericpapell123@gmail.com>
2023-06-06 17:02:25 -0500

    fetch function and font awesome added

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-function substratc(num){
-    return num -2;
-}
-function add(){
-    const x = 5+4;
-    return substratc(x)
-}
-
-add()
+const getQuote = async function(){
+    try{
+        const response = await fetch('https://jacintodesign.github.io/quotes-api/data/quotes.json')
+        const data = await response.json()
+        console.log(response)
+        console.log(data)
+    }
+    catch(err){
+        console.error(err)
+    }
+}()
